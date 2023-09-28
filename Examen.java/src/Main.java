a911c49574b0642d325c3c9df1b19a5734b544f1
Givin  Bahakoula <138108464+Givin-wish@users.noreply.github.com>
2023-09-28 12:53:57 +0200

    Add files via upload

--- Examen.java/src/Main.java
+++ Examen.java/src/Main.java
@@ -1,21 +1,74 @@
-import java.util.Scanner;
-abstract class CompteBancaire {
-    private String titulaire;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public abstract class CompteBancaire {
+    private Personne titulaire;
+    private String numeroCompte;
     private double solde;
+    private List<Transaction> transactions;
 
-
-    public CompteBancaire(String titulaire, double soldeInitial) {
-        this.titulaire = titulaire;
+    public CompteBancaire(String nom, String prenom, int age, String numeroCompte, double soldeInitial) {
+        this.numeroCompte = numeroCompte;
         this.solde = soldeInitial;
+        this.transactions = new ArrayList<>();
+        this.titulaire = new Personne(nom, prenom, age);
     }
-    public abstract void retirer(double montant);
 
     public abstract void deposer(double montant);
 
-    public double getSolde() {
+    public abstract void retirer(double montant);
 
+    public double getSolde() {
         return solde;
     }
 
+    public void afficherInformations() {
+        System.out.println("Titulaire : " + titulaire);
+        System.out.println("Numéro de compte : " + numeroCompte);
+        System.out.println("Solde : " + solde);
+    }
 
+    public void afficherHistoriqueTransactions() {
+        System.out.println("Historique des transactions pour le compte " + numeroCompte + ":");
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction.toString());
+        }
+    }
+
+    public void effectuerVirement(CompteBancaire compteDestinataire, double montant) {
+        if (montant <= solde) {
+            solde -= montant;
+            compteDestinataire.deposer(montant);
+            Transaction transaction = new Transaction(new Date(), montant, TypeTransaction.VIREMENT_SORTANT);
+            transactions.add(transaction);
+            compteDestinataire.transactions.add(new Transaction(new Date(), montant, TypeTransaction.VIREMENT_ENTRANT));
+        } else {
+            System.out.println("Solde insuffisant pour effectuer le virement.");
+        }
+    }
+}
+
+enum TypeTransaction {
+    DEPOSIT,
+    RETRAIT,
+    VIREMENT_SORTANT,
+    VIREMENT_ENTRANT
+}
+
+class Transaction {
+    private Date date;
+    private double montant;
+    private TypeTransaction typeTransaction;
+
+    public Transaction(Date date, double montant, TypeTransaction typeTransaction) {
+        this.date = date;
+        this.montant = montant;
+        this.typeTransaction = typeTransaction;
+    }
+
+    @Override
+    public String toString() {
+        return "Date : " + date + ", Montant : " + montant + ", Type : " + typeTransaction;
+    }
 }
